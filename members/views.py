202f879c3f48c6e8af576b51cd50aa848b8a2acd
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-10-22 18:11:38 +0600

    First Commit

---
+++ members/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from .models import Members
+
+
+def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template("index.html")
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template("add.html")
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse(index))
+
+
+def delete(request, id):
+    member = Members.objects.get(pk=id)
+    member.delete()
+    return HttpResponseRedirect(reverse(index))
